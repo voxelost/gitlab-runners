b8e312215245228ce022ed6a20730f26880d65fa
antoni boryna <borynaantoni@gmail.com>
2022-02-14 10:58:57 +0100

    refactor register command to noninteractive

--- setup.sh
+++ setup.sh
@@ -12,7 +12,17 @@ then
 
     docker build . --tag gitlab-runner-registerer
     docker run --rm -ti -v ${GITLAB_RUNNERS_CONFIG_DIR}:/etc/gitlab-runner \
-        gitlab-runner-registerer register --url ${GITLAB_INSTANCE_URL}
+        gitlab-runner-registerer register \
+            --non-interactive \
+            --url ${GITLAB_RUNNERS_URL} \
+            --registration-token ${GITLAB_RUNNERS_REGISTRATION_TOKEN} \
+            --executor ${GITLAB_RUNNERS_EXECUTOR} \
+            --docker-image ${GITLAB_RUNNERS_DOCKER_IMAGE} \
+            --description ${GITLAB_RUNNERS_DESCRIPTION} \
+            --tag-list ${GITLAB_RUNNERS_TAG_LIST} \
+            --run-untagged=${GITLAB_RUNNERS_RUN_UNTAGGED} \
+            --locked=${GITLAB_RUNNERS_LOCKED} \
+            --access-level=${GITLAB_RUNNERS_ACCESS_LEVEL}
     
     [ $? -eq 0 ] && echo "the runner should have its config file at ${GITLAB_RUNNERS_CONFIG_DIR}/config.toml"
 fi
