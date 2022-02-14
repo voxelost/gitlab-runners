887584f2718d3f26311f3afdbcf95e91791fad08
antoni boryna <borynaantoni@gmail.com>
2022-02-14 09:02:11 +0100

    refactor env var names

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [[ ! -f '.env' ]] 
+then
+    echo 'you need to add a .env file, see README for reference'
+    exit 2
+fi
+
 if [[ ${1} == '--register' ]]
 then
     source .env
 
     docker build . --tag gitlab-runner-registerer
-    docker run --rm -ti -v ${GITLAB_RUNNER_CONFIG_DIR}:/etc/gitlab-runner \
+    docker run --rm -ti -v ${GITLAB_RUNNERS_CONFIG_DIR}:/etc/gitlab-runner \
         gitlab-runner-registerer register --url ${GITLAB_INSTANCE_URL}
 fi
 
