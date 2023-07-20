5a452357225e4ce20072f1c4d3738567e6b1c045
Emilia <voxelost@proton.me>
2023-07-20 14:28:17 +0200

    add better feedback

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ then
     docker build . --tag gitlab-runner-registerer
     docker run --rm -ti -v ${GITLAB_RUNNERS_CONFIG_DIR}:/etc/gitlab-runner \
         gitlab-runner-registerer register --url ${GITLAB_INSTANCE_URL}
+    
+    [ $? -eq 0 ] && echo "the runner should have its config file at ${GITLAB_RUNNERS_CONFIG_DIR}/config.toml"
 fi
 
 docker-compose up -d --build
